1e57b635b91a565055c68581543fa1119c22f3fe
Alain Martin <amartin@keytrol.org>
2017-08-30 18:38:34 +0200

    WIP Add missing flags when building on Linux

--- cmake/Reprojucer.cmake
+++ cmake/Reprojucer.cmake
@@ -2555,11 +2555,17 @@ function(_FRUT_set_common_target_properties target)
     endif()
 
   elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    target_compile_definitions(${target} PRIVATE LINUX=1)
+
     foreach(config ${JUCER_PROJECT_CONFIGURATIONS})
       if(${JUCER_CONFIGURATION_IS_DEBUG_${config}})
         target_compile_definitions(${target} PRIVATE
           $<$<CONFIG:${config}>:DEBUG=1>
           $<$<CONFIG:${config}>:_DEBUG=1>
+        )
+        target_compile_options(${target} PRIVATE
+          $<$<CONFIG:${config}>:-g>
+          $<$<CONFIG:${config}>:-ggdb>
         )
       else()
         target_compile_definitions(${target} PRIVATE
